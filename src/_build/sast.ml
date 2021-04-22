ff42620a9a64b8d421c8c04c6d2b2f2b4a064b92
Emily <el2895@columbia.edu>
2021-04-21 21:13:51 -0400

    more tests

---
+++ src/_build/sast.ml
@@ -0,0 +1,81 @@
+(* Semantically-checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+type sexpr = typ * sx
+and sx =
+    SLitBool of bool 
+  | SLitInt of int 
+  | SLitString of string
+  | SLitRhythm of string 
+  | SLitNote of int * string
+  | SLitArray of sexpr list 
+  | SId of string
+  | SBinop of sexpr * op * sexpr
+  | SUniop of uop * sexpr
+  | SAssign of string * sexpr
+  | SCall of string * sexpr list
+  | SNoExpr
+
+type sstmt =
+    SBlock of sstmt list
+  | SExpr of sexpr
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sstmt
+  | SWhile of sexpr * sstmt 
+
+type sfunc_decl = {
+    sftype : typ;
+    sfname : string;
+    sparams : bind list;
+    svars : bind list;
+    sbody : sstmt list;
+  }
+
+type sprogram = bind list * sfunc_decl list
+
+(* Pretty-printing functions *)
+
+let rec string_of_sexpr (t, e) =
+  "(" ^ string_of_typ t ^ " : " ^ (match e with
+    SLitBool(true) -> "true"
+  | SLitBool(false) -> "false"
+  | SLitInt(i) -> string_of_int i
+  | SLitString(s) -> s
+  | SLitRhythm(r) -> r
+  | SLitNote(i, r) -> "<" ^ string_of_int i ^ " " ^ r ^ ">"
+  | SLitArray(el) -> "[" ^ String.concat ", " (List.map string_of_sexpr el) ^ "]"
+  | SId(s) -> s
+  | SBinop(e1, o, e2) ->
+      string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+  | SUniop(o, e) -> string_of_uop o ^ string_of_sexpr e
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | SCall(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SNoExpr -> ""
+				  ) ^ ")"				     
+
+let rec string_of_sstmt = function
+    SBlock(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
+  | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
+  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
+  | SIf(e, s, SBlock([])) -> "if " ^ string_of_sexpr e ^ "\n" ^ string_of_sstmt s
+  | SIf(e, s1, s2) ->  "if " ^ string_of_sexpr e ^ "\n" ^
+      string_of_sstmt s1 ^ "else\n" ^ string_of_sstmt s2
+  | SFor(e1, e2, s) ->
+      "for " ^ string_of_sexpr e1  ^ " in " ^ string_of_sexpr e2 ^ string_of_sstmt s
+  | SWhile(e, s) -> "while " ^ string_of_sexpr e ^ string_of_sstmt s
+
+let string_of_sfdecl fdecl =
+  string_of_typ fdecl.sftype ^ " " ^
+  fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sparams) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.svars) ^
+  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
+  "}\n"
+
+let string_of_sprogram (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_sfdecl funcs)
---
+++ src/_build/scanner.mll
@@ -0,0 +1,80 @@
+{ open Parser }
+
+let lowercase = ['a'-'z']
+let uppercase = ['A'-'Z']
+let letter = lowercase | uppercase
+let digit = ['0'-'9']
+let keys = ("C" | "C#" | "D" | "D#" | "E" | "F" | "F#" | "G" | "G#" | "A" | "A#" | "B")
+
+rule token = parse
+  [' ' '\t' '\r' '\n'] { token lexbuf }
+| ';' { SEP }
+| eof { EOF }
+(* SCOPING *)
+| '(' { LPAREN }
+| ')' { RPAREN }
+| '[' { LBRACK }
+| ']' { RBRACK }
+| '{' { LCURLY }
+| '}' { RCURLY }
+(* OPERATORS *)
+| '+' { PLUS }
+| '-' { MINUS }
+| '*' { TIMES }
+| '/' { DIVIDE }
+| '%' { MOD }
+| '=' { ASSIGN }
+| "==" { EQ }
+| "!=" { NEQ }
+| '<' { LT }
+| "<=" { LTE }
+| '>' { GT }
+| ">=" { GTE }
+| ',' { COMMA }
+| '$' { CONCAT }
+| '@' { BIND }
+| '^' { DUP }
+(* KEYWORDS *)
+(* DATA TYPES *)
+| "note" { NOTE }
+| "tone" { TONE }
+| "rhythm" { RHYTHM }
+| "int" { INT }
+| "bool" { BOOL }
+| "string" { STRING }
+| "none" { NONE }
+(* BOOLEAN LOGIC *)
+| "and" { AND }
+| "or" { OR }
+| "not" { NOT }
+(* PROGRAM STRUCTURE *)
+| "func" { FUNC }
+| "in" { IN }
+| "if" { IF }
+| "else" { ELSE }
+| "for" { FOR }
+| "while" { WHILE }
+| "return" { RETURN }
+(* LITERALS *)
+| ("wh" | "hf" | "qr" | "ei" | "sx") as lit { LIT_RHYTHM(lit) }
+| ("DEFAULT" | "BLUES" | "JAZZ") as lit { LIT_STYLE(lit) }
+| keys ("MAJ" | "MIN") as lit { LIT_KEY(lit) }
+| '"' (('\\' '"'| [^'"'])* as str) '"' { LIT_STRING(str) }
+| ['0'-'9']+ as lit { LIT_INT(int_of_string lit) }
+| "true"   { LIT_BOOL(true)  }
+| "false"  { LIT_BOOL(false) }
+(* IDENTIFIERS *)
+| (lowercase | '_') (letter | digit | '_')* as lit { ID(lit) }
+(* COMMENTS *)
+| "//" { commentLine lexbuf }
+| "/*" { commentBlock 0 lexbuf }
+
+and commentLine = parse
+| ('\n' | '\r' | "\r\n" | eof) { ENDLINE }
+| _ { commentLine lexbuf }
+
+and commentBlock level = parse
+| "/*" { commentBlock (level + 1) lexbuf }
+| "*/" { if level == 0 then token lexbuf 
+        else commentBlock (level - 1) lexbuf }
+| _ { commentBlock level lexbuf }
